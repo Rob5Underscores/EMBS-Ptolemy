416380ddd8b6bf7f58dd2fa353221d24a9482ca1
Rob Weddell <mc.robw@gmail.com>
2020-10-10 17:20:22 +0100

    Created base project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-embs_ptolemy
+#EMBS Ptolemy II Base 
+Clean copy of Ptolemy II v6.0.2
+
+Can import custom classes into Ptolemy: Graph > Instantiate Entity.
+
+Run Virgin (Ptolemy's UI) using `gradle run`
---
+++ src/main/java/ExamplePipe.java
@@ -0,0 +1,29 @@
+import ptolemy.actor.TypedAtomicActor;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.data.Token;
+import ptolemy.data.expr.Parameter;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+
+public class ExamplePipe extends TypedAtomicActor {
+
+    protected TypedIOPort input;
+    protected TypedIOPort output;
+    protected Parameter param;
+
+    public ExamplePipe(CompositeEntity container, String name) throws NameDuplicationException, IllegalActionException {
+        super(container, name);
+
+        input = new TypedIOPort(this, "inputlabel", true, false);
+        output = new TypedIOPort(this, "outputlabel", false, true);
+        param = new Parameter(this, "parameter name");
+        param.setExpression("20"); // initial value
+    }
+
+    public void fire() throws IllegalActionException{
+        Token t = input.get(0);
+        output.send(0, t);
+    }
+
+}
