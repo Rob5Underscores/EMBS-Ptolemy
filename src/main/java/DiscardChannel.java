c77753d706d6b6fbbd1a6fb94757d92e2cc35498
Rob Weddell <mc.robw@gmail.com>
2020-10-12 15:41:37 +0100

    .

--- src/main/java/DiscardChannel.java
+++ src/main/java/DiscardChannel.java
@@ -6,23 +6,38 @@ import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 
-public class ExamplePipe extends TypedAtomicActor {
+import java.util.Random;
+
+import static java.lang.Integer.parseInt;
+
+public class DiscardChannel extends TypedAtomicActor {
 
     protected TypedIOPort input;
     protected TypedIOPort output;
-    protected Parameter param;
+    protected Parameter discardPercent;
 
-    public ExamplePipe(CompositeEntity container, String name) throws NameDuplicationException, IllegalActionException {
+    /**
+     * Created for EMBS Exercise 2.3.1 Q5
+     * This actor discards % of the input tokens based on parameter.
+     */
+
+    public DiscardChannel(CompositeEntity container, String name) throws NameDuplicationException, IllegalActionException {
         super(container, name);
 
-        input = new TypedIOPort(this, "inputlabel", true, false);
-        output = new TypedIOPort(this, "outputlabel", false, true);
-        param = new Parameter(this, "parameter name");
-        param.setExpression("20"); // initial value
+        input = new TypedIOPort(this, "input", true, false);
+        output = new TypedIOPort(this, "output", false, true);
+        discardPercent = new Parameter(this, "Discard %");
+        discardPercent.setExpression("20"); // initial value
     }
 
     public void fire() throws IllegalActionException{
+        Random rand = new Random();
         Token t = input.get(0);
+        System.out.print(discardPercent.getValueAsString());
+        System.out.print(discardPercent.getExpression());
+        if(parseInt(discardPercent.getExpression()) >= rand.nextInt(100)) {
+            return;
+        }
         output.send(0, t);
     }
 
